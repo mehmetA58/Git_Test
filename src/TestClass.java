a77b5d7c3111c828b80985007d23afecbe3f6735
MehmetAkbayir <87230646+mehmetA58@users.noreply.github.com>
2021-08-18 14:30:35 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
+         System.out.println("GitHub dan merhaba!");
     }
 }
